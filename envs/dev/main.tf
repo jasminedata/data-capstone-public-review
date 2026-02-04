85e5f4a77854c65e31b4397c894623fac7fcfa5d
Jasmine Data <i_jasminejoy.data@stratpoint.com>
2026-02-04 11:05:08 +0800

    Add security groups module

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -28,3 +28,12 @@ module "s3_gateway_endpoint" {
   common_tags = local.common_tags
   name_prefix = "Data-FinalProject"
 }
+
+module "security_groups" {
+  source = "../../modules/security_groups"
+
+  vpc_id = module.vpc.vpc_id
+
+  common_tags = local.common_tags
+  name_prefix = "Data-FinalProject"
+}
